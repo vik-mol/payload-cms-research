2af4ee82b609af7acb5750754b4f4f93323ed2ce
Mol Victor <molvv@bcs.ru>
2025-04-29 00:55:27 +0400

    build: adaptation to FSD

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -4,7 +4,7 @@ import { getPayload } from 'payload'
 import React from 'react'
 import { fileURLToPath } from 'url'
 
-import config from '@/payload.config'
+import config from '@payload-config'
 import './styles.css'
 
 export default async function HomePage() {
